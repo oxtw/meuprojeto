c5ca199db047d6fdb1f046977d5e591db11dbcd7
miguel <weigertmiguel@hotmail.com>
2024-08-20 20:04:34 -0300

    Usando client componente dentro do server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,59 @@
-interface DataProps{
+import { OwnerRepo } from "@/components/OwnerRepo/index";
+
+interface DataProps {
   id: number;
   name: string;
   full_name: string;
-  owner:{
-      login: string;
-      id: number;
-      avatar_url: string;
-      url: string;
-  }
+  owner: {
+    login: string;
+    id: number;
+    avatar_url: string;
+    url: string;
+  };
 }
 
-async function delayFetch(url:string, delay:number){
-  await new Promise(resolve => setTimeout(resolve, delay));
+async function delayFetch(url: string, delay: number) {
+  await new Promise((resolve) => setTimeout(resolve, delay));
   const response = await fetch(url);
   return response.json();
 }
 
 // async function getData() {
- // https://api.github.com/users/oxtw/repos
+// https://api.github.com/users/oxtw/repos
 // const response = await fetch("https://api.github.com/users/oxtw/repos");
 
 // return response.json();
 // }
 
-async function getData(){
-  const data = await delayFetch("https://api.github.com/users/oxtw/repos", 1500)
+async function getData() {
+  const data = await delayFetch(
+    "https://api.github.com/users/oxtw/repos",
+    1500
+  );
   return data;
 }
 
 export default async function Home() {
-const data:DataProps[] = await getData();
+  const data: DataProps[] = await getData();
 
-return (
-  <div>
-    <h1>Página Home</h1>
-    <span> Bem vindo a pagina home</span>
-    <br />
+  return (
+    <div>
+      <h1>Página Home</h1>
+      <span> Bem vindo a pagina home</span>
+      <br />
 
-    <h3>Meus repositórios</h3>
-    {data.map((item)=> (
-      <div key={item.id}>
+      <h3>Meus repositórios</h3>
+      {data.map((item) => (
+        <div key={item.id}>
           <strong>Repositório: </strong> <a>{item.name}</a>
-          <br /><br />
-      </div>
-    ))}
-    
-  </div>
-);
+          <br />
+          <br />
+          <OwnerRepo 
+          avatar_url={item.owner.avatar_url}
+          name={item.owner.login}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
-
---
+++ src/components/OwnerRepo/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
+import Image from "next/image";
+
+interface OwnerRepoProps {
+  avatar_url: string;
+  name: string;
+}
+
+export function OwnerRepo({ avatar_url, name }: OwnerRepoProps) {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      {show && (
+        <>
+          <Image
+            src={avatar_url}
+            alt="Foto avatar do usuário"
+            width={34}
+            height={34}
+            style={{
+              borderRadius: 8,
+            }}
+          />
+          <strong>{name}</strong>
+        </>
+      )}
+
+      <button onClick={() => setShow(!show) }>
+        {show ? "Ocultar nome": "Mostrar nome"}
+      </button>
+    </div>
+  );
+}
